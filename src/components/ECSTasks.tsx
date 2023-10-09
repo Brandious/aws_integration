7c7f53ffb15e107f655c3d1e65f6b2a8ecc3271a
afacic <afacic@collectivemind.dev>
2023-10-09 11:10:45 +0200

    removedAPICALL

--- src/components/ECSTasks.tsx
+++ src/components/ECSTasks.tsx
@@ -2,19 +2,17 @@ import * as React from "react";
 import TaskCard from "./TaskCard";
 
 export default async function ECSTasks() {
-  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "";
+  // const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "";
 
-  const data = await fetch(`${BASE_URL}/api/awsservice`, { cache: "no-cache" });
-  if (!data.ok) {
-    return <div>Failed to load</div>;
-  }
-  const ecsDetails = await data.json();
+  // const data = await fetch(`${BASE_URL}/api/awsservice`, { cache: "no-cache" });
+  // if (!data.ok) {
+  //   return <div>Failed to load</div>;
+  // }
+  // const ecsDetails = await data.json();
 
   return (
     <div className="flex items-center justify-center min-h-screen  flex-wrap">
-      {ecsDetails.map((task: ExtractedTaskData, index: number) => (
-        <TaskCard task={task} key={index} />
-      ))}
+      <h1>Hello There</h1>
     </div>
   );
 }
